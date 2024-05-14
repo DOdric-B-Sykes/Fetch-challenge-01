29bc1e03712c2cc8021d5faaf0f5f7afc978d4fc
D_Sykes <Itachi_15_90@yahoo.com>
2024-05-13 23:13:05 -0500

    updated JS

--- js/script.js
+++ js/script.js
@@ -4,9 +4,29 @@ const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
 const getImage = async function (){
-    let res = await fetch ("https://picsum.photos/v2/list?limit=100");
+    let res = await fetch("https://picsum.photos/v2/list?limit=100");
     let images = await res.json();
-    console.log(images);
+    /* console.log(images); - commented out */
+    selectRandomImage(images);
 };
 
-getImage();
+
+const selectRandomImage = function (images){
+    let randomIndex = Math.floor(Math.random() * images.length);
+   /* console.log(randomIndex); - commented out */
+   let randomImage = images[randomIndex];
+   /* console.log(randomImage); - commented out */
+   displayImage(randomImage);
+};
+
+const displayImage = function (randomImage){
+    let author = randomImage.author;
+    let imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+}
+
+button.addEventListener("click", function (){
+    getImage();
+});
